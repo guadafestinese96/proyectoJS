6ad0e3569157604376f8d33c3fe2f1195d69f89e
guadafestinese96 <guadalupefestinese@gmail.com>
2024-01-30 23:22:11 -0300

    trabajando en el proyecto

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let opcion = parseInt(prompt("Selecciona tu zona: \n1)BUENOS AIRES \n2)NORTE: Jujuy, Formosa, Salta, Chaco, Tucumán, Catamarca, Santiago del Estero, Corrientes, Misiones, La Rioja.  \n3)CENTRO: Entre Ríos, Santa Fé, Córdoba, San Juan, San Luis, Mendoza, La Pampa. \n4)SUR: Neuquén, Río Negro, Chubut, Santa Cruz, Tierra del Fuego."))
+switch (opcion) {
+    case 1:
+        let op = parseInt(prompt("Si es de CABA ingrese 1, si es de GBA ingrese 2"));
+        switch (op) {
+            case 1:
+                alert("El envío por motomensajería en CABA vale $2500");
+                break;
+            case 2:
+                alert("El envío por motomensajería en GBA vale $4000");
+                break;
+            default: 
+                alert("Opción inválida");
+                break;
+        }
+        break;
+    case 2:
+        alert("El envío por Vía Cargo al Norte de Argentina vale $8500");
+        break;
+    case 3:
+        alert("El envío al Centro de Argentina por Vía Cargo vale $7500");
+        break;
+    case 4:
+        alert("El envío al Sur de Argentina vale $10.500");
+        break;
+    default:
+        alert("OPCION INVALIDA");
+        break;
+}
